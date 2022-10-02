18f614620a724cd0ae2ce1e0c2498a41a628991d
Sofia Lumme <sofia.lumme@gmail.com>
2022-10-03 00:28:13 +0300

    tehtävä valmis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState({ description: '', date: '' });
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({ ...todo, [event.target.name]: event.target.value });
+  }
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  }
+
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((todo, i) => i !== index));
+  }
+
+  const itemRows = todos.map((todo, index) =>
+    <tr key={index}>
+      <td>{todo.date}</td>
+      <td>{todo.description}</td>
+      <td><button onClick={() => deleteTodo(index)}>Delete</button></td>
+    </tr>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Simple Todolist</h1>
+      <h2>Add todo:</h2>
+      <label>Description: </label>
+      <input type="text" name="description" onChange={inputChanged} value={todo.description} />
+      <label>Date: </label>
+      <input type="text" name="date" onChange={inputChanged} value={todo.date} />
+      <button onClick={addTodo}>Add</button>
+      <table>
+        <tbody>
+          <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th></th>
+          </tr>
+          {itemRows}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
